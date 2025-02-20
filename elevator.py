47d5f968c5b07e5d0b4c63d5293312826072a40a
ofirglazer <ofirglazer@gmail.com>
2025-02-20 20:07:31 +0200

    simulated 1 calls

--- elevator.py
+++ elevator.py
@@ -1,4 +1,4 @@
-#import pygame
+# import pygame
 # import math
 from enum import Enum
 
@@ -10,7 +10,8 @@ class State(Enum):
 	UP_SLOW = 3
 	DOWN_SLOW = 4
 
-#pygame.init()
+
+# pygame.init()
 '''
 WIDTH, HEIGHT = 400, 800
 WHITE = (255, 255, 255)
@@ -27,42 +28,58 @@ class Controller:
 
 
 class Elevator:
-    def _init_(self):
-        self.floor =  0
-        self.state = State.IDLE
-        self.goal = 0
-        
-    def _str_(self):
-        return f"Elevator is at {self.floor} and is in {self.state}"
-        
-    def goto(self, floor):
-        if self.state == State.IDLE:
-        	self.goal = floor
-        	if self.goal > self.floor:
-        		self.state = State.UP
-        	elif self.goal < self.floor:
-        		self.state = State.DOWN
-        	return 0
-        else:
-        	return -1
-        	
-     def 
+	vel = 0.2
+	def __init__(self):
+		self.floor = 0
+		self.state = State.IDLE
+		self.goal = 0
+
+	def __str__(self):
+		return f"Elevator is at {self.floor} and is in {self.state}"
+
+	def goto(self, floor):
+		if self.state == State.IDLE:
+			self.goal = floor
+			if self.goal > self.floor:
+				self.state = State.UP
+			elif self.goal < self.floor:
+				self.state = State.DOWN
+			return 0
+		else:
+			return -1
+
+	def update(self):
+		if self.state == State.UP:
+			self.floor += self.vel
+		elif self.state == State.DOWN:
+			self.floor -= self.vel
+		self.floor = round(self.floor, 1)
+
+		if self.floor == self.goal:
+			self.state = State.IDLE
+
 
 '''    def draw(self):
-        pygame.draw.circle(win, WHITE, (WIDTH // 2, HEIGHT // 2), 30)'''
+		pygame.draw.circle(win, WHITE, (WIDTH // 2, HEIGHT // 2), 30)'''
 
 
 def main():
-	
+
 	elev = Elevator()
 	print(elev)
 	time_sec = 0
-	running = True
-	
-	while time_sec <= 10:
-	     time_sec += 1
-	     print(f"time: {time_sec}, elevator state is {elev.state}")
-      
-      
-if _name_ == "_main_":
-    main()
+	# running = True
+
+	while time_sec <= 50:
+		time_sec += 1
+		if time_sec == 10:
+			elev.goto(3)
+		elif time_sec == 30:
+			elev.goto(1)
+
+		elev.update()
+		print(f"time: {time_sec}, elevator is at {elev.floor} and is {elev.state}")
+
+
+if __name__ == "__main__":
+	main()
